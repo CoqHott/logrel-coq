7ef2609f76b717abae9812ced350321ef1ebfcfd
arthur-adjedj <arthur.adjedj@gmail.com>
2022-08-13 15:26:53 +0200

    Progress on Symmetry

--- Shapeview.v
+++ Shapeview.v
@@ -1,26 +1,46 @@
 Require Import MLTTTyping LogicalRelation.
 
-Check LRPi.
+Set Universe Polymorphism.
 
-Inductive ShapeView Γ (l l' : TypeLevel) : 
-    forall A B, [Γ ||-< l | A] -> [Γ ||-< l' | B] -> Type 
+
+
+Inductive ShapeView0 Γ : 
+    forall A B, [Γ ||-< zero | A] -> [Γ ||-< zero | B] -> Type 
 :=
-    | SVU {la l_a Γa lb l_b Γb} :
-        ShapeView Γ l l' U U 
-         (LRU_ Γa la l_a)
-         (LRU_ Γb lb l_b)
-
     | SVne {A B} neA neB :
-        ShapeView Γ l l' A B
-        (LRne_ _ _ neA)
-        (LRne_ _ _ neB)
+        ShapeView0 Γ A B
+        (LRne_ _ zero  neA)
+        (LRne_ _ zero neB)
 
     | SVPi {A B} Π0A Π0B Π1A Π1B:
-        ShapeView Γ l l' A B
+        ShapeView0 Γ A B
         (LRPi_ _ _ Π0A Π1A)
         (LRPi_ _ _ Π0B Π1B)
          
-    | SVemb01 {A B p q} (leq : l = one) :
-      ShapeView Γ zero l' A B p q ->
-      ShapeView Γ l l' A B (LREmb_ _  (LESubst leq) p) q   
-          . 
+
+          .
+
+Inductive ShapeView1 Γ : 
+    forall A B, [Γ ||-< one | A] -> [Γ ||-< one | B] -> Type 
+:=
+    (*| SVU {l_a Γa l_b Γb} :
+      ShapeView Γ _ _
+        (LRValidMk _ _ _
+            (LRU (LogRelRec _) Γa _ l_a))
+        (LRValidMk _ _ _
+            (LRU (LogRelRec _) Γb _ l_b))
+
+    | SVne {A B} neA neB :
+      ShapeView Γ A B
+        (LRne_ _ _ neA)
+        (LRne_ _ _  neB)
+
+    | SVPi {A B} Π0A Π0B Π1A Π1B :
+        ShapeView Γ A B
+        (LRPi_ _ _ Π0A Π1A)
+        (LRPi_ _ _ Π0B Π1B)*)
+         
+    | SVemb01 {A B p q} (leq : l = one):
+       @ShapeView Γ l l' A B p q ->
+        ShapeView Γ A B (LREmb_ _  (LESubst leq) p) q.
+(*universe error.....*)      
