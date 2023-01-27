6209e584fc6ef98a85c85b558148f4e90961c4bb
loic-p <loic.pujet@ens.fr>
2023-01-27 01:17:12 +0100

    distinguish validity predicates and validity adequacy

--- theories/Validity.v
+++ theories/Validity.v
@@ -18,11 +18,11 @@ Ltac substitution := eauto with substitution.
   three outputs (validTy, validSubst and eqSubst) *)
 
   #[universes(polymorphic)]Definition VRel@{i j | i < j +} `{ta : tag} `{!WfContext ta} :=
-  context ->
-  (* (TypeLevel -> term -> Type@{i}) -> *)
-  forall (validSubst : forall (Δ : context) (σ : nat -> term) (wfΔ : [|- Δ]), Type@{i}),
-  (forall (Δ : context) (σ σ' : nat -> term) (wfΔ : [|- Δ ]), validSubst Δ σ wfΔ -> Type@{i}) ->
-  Type@{j}.
+  forall 
+    (Γ : context)
+    (validSubst : forall (Δ : context) (σ : nat -> term) (wfΔ : [|- Δ]), Type@{i})
+    (eqSubst : forall (Δ : context) (σ σ' : nat -> term) (wfΔ : [|- Δ ]), validSubst Δ σ wfΔ -> Type@{i}) 
+    , Type@{j}.
 
 (* An VPack contains the data corresponding to the codomain of RedRel seen as a functional relation *)
 
@@ -30,7 +30,6 @@ Module VPack.
 
   #[universes(polymorphic)] Record VPack@{i} `{ta : tag} `{!WfContext ta} {Γ : context} :=
   {
-    (* validTy : TypeLevel -> term -> Type@{i}; *)
     validSubst : forall (Δ : context) (σ : nat -> term) (wfΔ : [|- Δ]), Type@{i} ;
     eqSubst : forall (Δ : context) (σ σ' : nat -> term) (wfΔ : [|- Δ ]), validSubst Δ σ wfΔ -> Type@{i} ;
   }.
@@ -41,14 +40,13 @@ End VPack.
 
 Export VPack(VPack,Build_VPack).
 
-(* Notation "[ P | Γ ||-v< l > A ]" := (@VPack.validTy Γ l A P). *)
-Notation "[ P | Δ ||-v σ : Γ | wfΔ ]" := (@VPack.validSubst Γ Δ σ wfΔ) (at level 0, P, Δ, σ, Γ, wfΔ at level 50).
-Notation "[ P | Δ ||-v σ ≅ σ' : Γ | wfΔ | vσ ]" := (@VPack.eqSubst Γ Δ σ σ' wfΔ vσ P) (at level 0, P, Δ, σ, σ', Γ, wfΔ, vσ at level 50).
+Notation "[ P | Δ ||-v σ : Γ | wfΔ ]" := (@VPack.validSubst _ _ Γ P Δ σ wfΔ) (at level 0, P, Δ, σ, Γ, wfΔ at level 50).
+Notation "[ P | Δ ||-v σ ≅ σ' : Γ | wfΔ | vσ ]" := (@VPack.eqSubst _ _ Γ P Δ σ σ' wfΔ vσ) (at level 0, P, Δ, σ, σ', Γ, wfΔ, vσ at level 50).
 
 (* An VPack it adequate wrt. a VRel when its three unpacked components are *)
 #[universes(polymorphic)] Definition VPackAdequate@{i j} `{ta : tag} `{!WfContext ta} {Γ : context}
   (R : VRel@{i j}) (P : VPack@{i} Γ) : Type@{j} :=
-  R Γ (*P.(VPack.validTy)*) P.(VPack.validSubst) P.(VPack.eqSubst).
+  R Γ P.(VPack.validSubst) P.(VPack.eqSubst).
 
 Arguments VPackAdequate _ _ /.
 
@@ -72,40 +70,32 @@ Export VAd(VAdequate,Build_VAdequate).
 Coercion VAd.pack : VAdequate >-> VPack.
 Coercion VAd.adequate : VAdequate >-> VPackAdequate.
 
-
 Notation "[ R | ||-v Γ ]"                            := (VAdequate Γ R) (at level 0, R, Γ at level 50).
 Notation "[ R | Δ ||-v σ : Γ | RΓ | wfΔ ]"           := (RΓ.(VPack.validSubst) Δ σ wfΔ) (at level 0, R, Δ, σ, Γ, RΓ, wfΔ at level 50).
 Notation "[ R | Δ ||-v σ ≅ σ' : Γ | RΓ | wfΔ | vσ ]" := (RΓ.(VPack.eqSubst) Δ σ σ' wfΔ vσ) (at level 0, R, Δ, σ, σ', Γ, RΓ, wfΔ, vσ at level 50).
 
-Record typeValidityPack `{ta : tag} `{!WfContext ta}
+Record typeValidity `{ta : tag} `{!WfContext ta}
   `{!WfType ta} `{!Typing ta} `{!ConvType ta}
   `{!ConvTerm ta} `{!ConvNeu ta} `{!RedType ta} `{!RedTerm ta}
-  {Γ : context} {R : VRel} {RΓ : [ R | ||-v Γ ]}
-  {l : TypeLevel} {A : term}
-  {Δ : context} {σ : nat -> term} {wfΔ : [|- Δ ]} {vσ : [ R | Δ ||-v σ : Γ | RΓ | wfΔ]} :=
+  {Γ : context} {VΓ : VPack Γ}
+  {l : TypeLevel} {A : term} :=
   {
-    validTy : [ Δ ||-< l > A [ σ ] ] ;
-    validTyExt : forall (σ' : nat -> term)
-      (vσ' : [R| Δ ||-v σ' : Γ | RΓ | wfΔ])
-      (vσσ' : [R| Δ ||-v σ ≅ σ' : Γ | RΓ | wfΔ | vσ]),
-      [ Δ ||-< l > A [ σ ] ≅ A [ σ' ] | validTy ]
+    validTy : forall (Δ : context) (σ : nat -> term)
+      (wfΔ : [|- Δ ]) 
+      (vσ : [ VΓ | Δ ||-v σ : Γ | wfΔ ])
+      , [ Δ ||-< l > A [ σ ] ] ;
+    validTyExt : forall (Δ : context) (σ σ' : nat -> term)
+      (wfΔ : [|- Δ ])
+      (vσ  : [ VΓ | Δ ||-v σ  : Γ | wfΔ ])
+      (vσ' : [ VΓ | Δ ||-v σ' : Γ | wfΔ ])
+      (vσσ' : [ VΓ | Δ ||-v σ ≅ σ' : Γ | wfΔ | vσ ])
+      , [ Δ ||-< l > A [ σ ] ≅ A [ σ' ] | validTy Δ σ wfΔ vσ ]
   }.
 
-Arguments typeValidityPack : clear implicits.
-Arguments typeValidityPack {_ _ _ _ _ _ _ _ _}.
-
-
-Definition typeValidity `{ta : tag} `{!WfContext ta}
-  `{!WfType ta} `{!Typing ta} `{!ConvType ta}
-  `{!ConvTerm ta} `{!ConvNeu ta} `{!RedType ta} `{!RedTerm ta}
-  (Γ : context) (R : VRel) (RΓ : [ R | ||-v Γ ]) :
-  TypeLevel -> term -> Type :=
-  fun l A =>
-    forall (Δ : context) (σ : nat -> term) (wfΔ : [|- Δ ]) (vσ : [ R | Δ ||-v σ : Γ | RΓ | wfΔ]),
-    typeValidityPack Γ R RΓ l A Δ σ wfΔ vσ.
+Arguments typeValidity : clear implicits.
+Arguments typeValidity {_ _ _ _ _ _ _ _ _}.
     
-Notation "[ R | Γ ||-v< l > A | RΓ ]" := (typeValidity Γ R RΓ l A) (at level 0, R, Γ, l, A, RΓ at level 50).
-
+Notation "[ P | Γ ||-v< l > A ]" := (typeValidity Γ P l A) (at level 0, P, Γ, l, A at level 50).
 
 Definition emptyValidSubst `{ta : tag} `{!WfContext ta} : forall (Δ : context) (σ : nat -> term) (wfΔ : [|- Δ]), Type := fun _ _ _ => unit.
 Definition emptyEqSubst `{ta : tag} `{!WfContext ta} : forall (Δ : context) (σ σ' : nat -> term) (wfΔ : [|- Δ]), emptyValidSubst Δ σ wfΔ -> Type := fun _ _ _ _ _ => unit.
@@ -115,21 +105,21 @@ Section snocValid.
   Context `{ta : tag} `{!WfContext ta}
   `{!WfType ta} `{!Typing ta} `{!ConvType ta}
   `{!ConvTerm ta} `{!ConvNeu ta} `{!RedType ta} `{!RedTerm ta}
-  {Γ : context} {R : VRel} {RΓ : [ R | ||-v Γ ]} {A : term} {l : TypeLevel}
-  {vA : [R | Γ ||-v< l > A | RΓ ]}.
+  {Γ : context} {VΓ : VPack Γ} {A : term} {l : TypeLevel}
+  {vA : [ VΓ | Γ ||-v< l > A ]}.
 
   Record snocValidSubst {Δ : context} {σ : nat -> term} {wfΔ : [|- Δ]} : Type := 
     {
-      validTail : [ R | Δ ||-v ↑ >> σ : Γ | RΓ | wfΔ ] ;
-      validHead : [ Δ ||-< l > σ var_zero : A[↑ >> σ] | validTy (vA Δ (↑ >> σ) wfΔ validTail) ]
+      validTail : [ VΓ | Δ ||-v ↑ >> σ : Γ | wfΔ ] ;
+      validHead : [ Δ ||-< l > σ var_zero : A[↑ >> σ] | validTy vA Δ (↑ >> σ) wfΔ validTail ]
     }.
 
   Arguments snocValidSubst : clear implicits.
 
   Record snocEqSubst {Δ : context} {σ σ' : nat -> term} {wfΔ : [|- Δ]} {vσ : snocValidSubst Δ σ wfΔ} : Type :=
     {
-      eqTail : [ R | Δ ||-v ↑ >> σ ≅ ↑ >> σ' : Γ | RΓ | wfΔ | validTail vσ ] ;
-      eqHead : [ Δ ||-< l > σ var_zero ≅ σ' var_zero : A[↑ >> σ] | validTy (vA Δ (↑ >> σ) wfΔ (validTail vσ)) ]
+      eqTail : [ VΓ | Δ ||-v ↑ >> σ ≅ ↑ >> σ' : Γ | wfΔ | validTail vσ ] ;
+      eqHead : [ Δ ||-< l > σ var_zero ≅ σ' var_zero : A[↑ >> σ] | validTy vA Δ (↑ >> σ) wfΔ (validTail vσ) ]
     }.
   
 End snocValid.
@@ -147,9 +137,10 @@ Inductive VR `{ta : tag}
   `{RedType ta} `{RedTerm ta} : VRel :=
   | VREmpty : VR ε emptyValidSubst emptyEqSubst
   | VRSnoc : forall {Γ A l}
-    (RΓ : [ VR | ||-v Γ])
-    (vA : [ VR | Γ ||-v< l > A | RΓ ]),
-    VR (Γ ,, vass (Build_aname String.EmptyString) A) (snocValidSubst Γ VR RΓ A l vA) (snocEqSubst Γ VR RΓ A l vA).
+    (VΓ : VPack Γ)
+    (VΓad : VPackAdequate VR VΓ)
+    (vA : [ VΓ | Γ ||-v< l > A ]),
+    VR (Γ ,, vass (Build_aname String.EmptyString) A) (snocValidSubst Γ VΓ A l vA) (snocEqSubst Γ VΓ A l vA).
 
 
 
