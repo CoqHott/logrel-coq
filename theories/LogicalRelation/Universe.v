ae930cc41944ecea84a584ecc2adaf68ad2cfd66
loic-p <loic.pujet@ens.fr>
2023-03-07 16:19:19 +0100

    Update Universe.v with new cumulativity

--- theories/LogicalRelation/Universe.v
+++ theories/LogicalRelation/Universe.v
@@ -8,31 +8,6 @@ Set Printing Universes.
 Section UniverseReducibility.
   Context `{GenericTypingProperties}.
 
-  (* These proofs are somewhat hack-ish and do not generalize properly to arbitrarily many universes *)
-  (* In particular, they exploit the following lemma, which proves "cumulativity" of LogRel0, but
-     unfortunately this lemma does not generalize to LogRel l *)
-  (* In proper generality, one would need to show that
-     LogRel@{i j k} l Γ A EqTy RedTm EqTm -> LogRel@{j k l} l Γ A EqTy' RedTm' EqTm'
-     where EqTy' RedTm' EqTm' are equivalent but non definitionally equal predicates *)
-
-  Definition LogRel0_Cumulative@{i j k l} {Γ A} {EqTy RedTm : term -> Type@{j}} {EqTm : term -> term -> Type@{j}}
-    (lr : LogRel0@{i j k} Γ A EqTy RedTm EqTm) : LogRel0@{j k l} Γ A EqTy RedTm EqTm.
-  Proof.
-    induction lr as [ ?? HU | ? ? neA | ? ? ? ? domAd codAd ].
-    - exact (match elim HU.(URedTy.lt) with end).
-    - exact (LRne _ neA).
-    - eapply LRPi@{j k l}; now unshelve econstructor.
-  Defined.
-
-  Definition TyRed_Cumulative@{i j k l} {Γ} {A} (rA : [ LogRel0@{i j k} | Γ ||- A ])
-      : [ LogRel0@{j k l} | Γ ||- A ].
-  Proof.
-    destruct rA as [Pack PackAd].
-    unshelve econstructor.
-    - exact Pack.
-    - exact (LogRel0_Cumulative@{i j k l} PackAd).
-  Defined.
-
   Lemma redUOne {Γ l A} : [Γ ||-<l> A] -> [Γ ||-U<one> U].
   Proof.
     intros ?%escape_; econstructor; [easy| gen_typing|eapply redtywf_refl; gen_typing].
@@ -43,8 +18,8 @@ Section UniverseReducibility.
   Proof.
     assert [ LogRel@{i j k l} one | Γ ||- A : U | LRU_@{i j k l} (redUOne rU)]
              as [ _ _ _ _ hA ] by irrelevance.
-    exact (TyRed_Cumulative hA).
-  Defined.
+    exact (LRCumulative hA).
+  Qed.
 
   Lemma UnivEq@{i j k l} {Γ A l} l' (rU : [ LogRel@{i j k l} l | Γ ||- U]) (rA : [ LogRel@{i j k l} l | Γ ||- A : U | rU])
     : [ LogRel@{i j k l} l' | Γ ||- A].
@@ -54,13 +29,12 @@ Section UniverseReducibility.
     - econstructor. eapply LR_embedding.
       + exact Oi.
       + apply (UnivEq' rU rA).
-  Defined.
+  Qed.
 
   Lemma UnivEqEq@{i j k l} {Γ A B l l'} (rU : [ LogRel@{i j k l} l | Γ ||- U ]) (rA : [LogRel@{i j k l} l' | Γ ||- A ]) (rAB : [ LogRel@{i j k l} l | Γ ||- A ≅ B : U | rU ])
     : [ LogRel@{i j k l} l' | Γ ||- A ≅ B | rA ].
   Proof.
     assert [ LogRel@{i j k l} one | Γ ||- A ≅ B : U | LRU_@{i j k l} (redUOne rU) ] as [ _ _ _ hA _ hAB ] by irrelevance.
-    change ([ TyRed_Cumulative@{i j k l} hA | Γ ||- A ≅ B ]) in hAB.
     eapply LRTyEqIrrelevant. exact hAB.
   Qed.
 
@@ -75,7 +49,7 @@ Section UniverseReducibility.
 
   Lemma univTmTy@{h i j k l} {Γ l A} (RU : [Γ ||-<l> U]) :
     [LogRel@{i j k l} l | Γ ||- A : U | RU] -> [LogRel@{h i j k} (URedTy.level (invLRU RU)) | Γ ||- A].
-  Proof. 
+  Proof.
     intros h; apply univTmTy'.
     irrelevance.
   Qed.
