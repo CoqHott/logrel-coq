c4f8b1cdbc817f6937597c4db8dd9d10728edaa7
Kenji Maillard <kenji@maillard.blue>
2023-03-17 18:31:34 +0100

    reducibility of composition of simple functions

--- theories/LogicalRelation/SimpleArr.v
+++ theories/LogicalRelation/SimpleArr.v
@@ -127,4 +127,96 @@ Section SimpleArrow.
     now bsimpl.
   Qed.
 
+  Lemma arr_wk {Γ Δ A B} {ρ : Δ ≤ Γ} : arr A⟨ρ⟩ B⟨ρ⟩ = (arr A B)⟨ρ⟩.
+  Proof. cbn; now bsimpl. Qed.
+
+  Lemma wkRedArr {Γ l A B f} 
+    (RA : [Γ ||-<l> A]) 
+    (RB : [Γ ||-<l> B]) 
+    {Δ} (ρ : Δ ≤ Γ) (wfΔ : [|- Δ]) :
+    [Γ ||-<l> f : arr A B | ArrRedTy RA RB] ->
+    [Δ ||-<l> f⟨ρ⟩ : arr A⟨ρ⟩ B⟨ρ⟩ | ArrRedTy (wk ρ wfΔ RA) (wk ρ wfΔ RB)].
+  Proof.
+    intro; irrelevance0.
+    2: unshelve eapply wkTerm; cycle 3; tea.
+    symmetry; apply arr_wk.
+  Qed.
+
+  Lemma compred {Γ l A B C f g} 
+    (RA : [Γ ||-<l> A]) 
+    (RB : [Γ ||-<l> B]) 
+    (RC : [Γ ||-<l> C]) :
+    [Γ ||-<l> f : arr A B | ArrRedTy RA RB] ->
+    [Γ ||-<l> g : arr B C | ArrRedTy RB RC] ->
+    [Γ ||-<l> comp A g f : arr A C | ArrRedTy RA RC].
+  Proof.
+    intros Rf Rg.
+    normRedΠin ΠAB Rf; normRedΠin ΠBC Rg; normRedΠ ΠAC. 
+    assert (h : forall Δ a (ρ : Δ ≤ Γ) (wfΔ : [|- Δ]) RA (ha : [Δ ||-<l> a : A⟨ρ⟩ | RA]),
+      [Δ ||-<l> tApp g⟨ρ⟩ (tApp f⟨ρ⟩ a) : _ | wk ρ wfΔ RC]).
+    1:{
+      intros; eapply simple_appTerm; [|eapply simple_appTerm; tea].
+      1,2: eapply wkRedArr; irrelevance.
+      Unshelve. 1: eapply wk. all: tea.
+    }
+    assert (heq : forall Δ a b (ρ : Δ ≤ Γ) (wfΔ : [|- Δ]) RA
+      (ha : [Δ ||-<l> a : A⟨ρ⟩ | RA]) (hb : [Δ ||-<l> b : A⟨ρ⟩ | RA])
+      (ha : [Δ ||-<l> a ≅ b: A⟨ρ⟩ | RA]),
+      [Δ ||-<l> tApp g⟨ρ⟩ (tApp f⟨ρ⟩ a) ≅ tApp g⟨ρ⟩ (tApp f⟨ρ⟩ b) : _ | wk ρ wfΔ RC]).
+    1:{
+      intros.
+      eapply simple_appcongTerm; [| | |eapply simple_appcongTerm; tea].
+      1,4: eapply LREqTermRefl_; eapply wkRedArr; irrelevance.
+      1,2: eapply simple_appTerm; tea; eapply wkRedArr; irrelevance.
+      Unshelve. 1: eapply wk. all: tea.
+    }
+    escape.
+    assert (beta : forall Δ a (ρ : Δ ≤ Γ) (wfΔ : [|- Δ]) RA (ha : [Δ ||-<l> a : A⟨ρ⟩ | RA]),
+      [Δ ||-<l> tApp (comp A g f)⟨ρ⟩ a : _ | wk ρ wfΔ RC] ×
+      [Δ ||-<l> tApp (comp A g f)⟨ρ⟩ a ≅ tApp g⟨ρ⟩ (tApp f⟨ρ⟩ a) : _ | wk ρ wfΔ RC]).
+    1:{
+      intros; cbn. 
+      assert (eq : forall t : term, t⟨↑⟩⟨upRen_term_term ρ⟩ = t⟨ρ⟩⟨↑⟩) by (intros; now asimpl).
+      do 2 rewrite eq.
+      eapply redSubstTermOneStep.
+      + eapply ty_simple_app.
+        4: cbn in *; now escape.
+        3: eapply ty_comp.
+        6,7: erewrite arr_wk; eapply ty_wk; tea.
+        1-5: eapply wft_wk; tea.
+      + eapply osredtm_comp_beta.
+        6: cbn in *; now escape.
+        5: erewrite arr_wk; eapply ty_wk; tea.
+        4: eapply typing_meta_conv.
+        4: eapply ty_wk; tea.
+        4: now rewrite <- arr_wk.
+        1-3: now eapply wft_wk.
+      + now eapply h.
+    }
+    econstructor.
+    - eapply redtmwf_refl.
+      eapply ty_comp; cycle 2; tea.
+    - constructor.
+    - cbn. eapply convtm_comp; cycle 3; tea.
+      erewrite <- wk1_ren_on.
+      eapply escapeEqTerm.
+      eapply LREqTermRefl_.
+      do 2 erewrite <- wk1_ren_on.
+      eapply h.
+      eapply var0; now bsimpl.
+      Unshelve. 1: gen_typing.
+      eapply wk; tea; gen_typing.
+    - intros; cbn.
+      irrelevance0.
+      2: unshelve eapply beta; tea.
+      bsimpl; now rewrite rinstInst'_term.
+    - intros; irrelevance0; cycle 1.
+      1: eapply transEqTerm;[|eapply transEqTerm].
+      + unshelve eapply beta; tea.
+      + unshelve eapply heq; tea.
+      + eapply LRTmEqSym.
+        unshelve eapply beta; tea.
+      + cbn; bsimpl; now rewrite rinstInst'_term.
+  Qed. 
+
 End SimpleArrow.
