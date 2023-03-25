35d26887068eb188c4d34fa58b72c590f8a6f6ad
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2023-03-25 20:51:07 +0100

    Fill in the missing admits.

--- theories/AlgorithmicConvProperties.v
+++ theories/AlgorithmicConvProperties.v
@@ -764,8 +764,40 @@ Qed.
     - intros_bn.
       1-2: gen_typing.
       now do 2 econstructor.
-    - admit.
-    - admit.
+    - intros ???? [] [] [].
+      assert [Γ,, tNat |-[de] P].
+      { now apply typing_sound. }
+      assert [Γ |-[de] hz : P[tZero..]] by eauto using inf_conv_decl.
+      assert [Γ |-[de] hs : elimSuccHypTy P].
+      { eapply inf_conv_decl; eassumption. }
+      assert [ |-[de] Γ] by boundary.
+      constructor.
+      + assumption.
+      + admit.
+      + constructor; tea.
+        now constructor.
+      + eassumption.
+      + eapply termConvRed; [reflexivity| |reflexivity|].
+        * eapply redSuccAlg; [|reflexivity].
+          apply UntypedReduction.natElimZero.
+        * Search concl:ConvTermRedAlg.
+reflexivity.
+        econstructor; [|].
+
+    - intros ????? [] [] [] [].
+      assert [Γ,, tNat |-[de] P].
+      { now apply typing_sound. }
+      assert [Γ |-[de] hz : P[tZero..]] by eauto using inf_conv_decl.
+      assert [Γ |-[de] hs : elimSuccHypTy P].
+      { eapply inf_conv_decl; eassumption. }
+      assert [Γ |-[de] n : tNat] by eauto using inf_conv_decl.
+      assert [ |-[de] Γ] by boundary.
+      constructor.
+      + assumption.
+      + admit.
+      + constructor; tea.
+        constructor; tea.
+      + admit.
   Admitted.
 
   #[export, refine] Instance ConvNeuAlgProperties : ConvNeuProperties (ta := bn) := {}.
