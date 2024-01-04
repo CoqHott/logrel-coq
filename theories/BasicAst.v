447c8c7ae93c440ac3f8a82d6877c8639db200f1
Meven <mgapb2@cam.ac.uk>
2024-01-04 14:03:10 +0100

    cleaning up structuration

--- theories/BasicAst.v
+++ theories/BasicAst.v
@@ -1,6 +1,5 @@
 (** * LogRel.BasicAst: definitions preceding those of the AST of terms: sorts, binder annotations… *)
 From Coq Require Import String Bool.
-From LogRel.AutoSubst Require Import core unscoped.
 
 Inductive sort :=
   | set : sort.
